81276f5bc65a4b8ff7b9a40cadbedefcbe317957
Israel Pina <israelpi004@gmail.com>
2022-01-21 10:48:35 -0500

    changes

--- setup.h
+++ setup.h
@@ -1,6 +1,6 @@
 #include <ctype.h>
 
-int setup(int user_board[10][10]);
+int setup(int user_board[10][10], int attack_board[10][10]);
 int carrier(int board[10][10]);
 int battleship(int board[10][10], int coords[4][2]);
 int cruiser(int board[10][10], int coords[3][2]);
