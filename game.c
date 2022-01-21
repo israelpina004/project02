7f9acef41a9b6c4d42bfd86accafb068ff9e8c1a
Israel Pina <israelpi004@gmail.com>
2022-01-21 00:41:10 -0500

    added

---
+++ game.c
@@ -0,0 +1,64 @@
+#include "networking.h"
+#include "game.h"
+
+int game(int user_board[10][10], int attack_board[10][10], int socket_fd) {
+  int i, j;
+  char s[100];
+
+  printf("\nYour attack board:\n", );
+  for(i = 0; i < 10; i++) {
+    for(j = 0; j < 10; j++) {
+      printf("%d ", attack_board[j][i]);
+    }
+    printf("\n");
+  }
+
+  printf("\nEnter a message: ");
+  fgets(s, 100, stdin);
+  write(socket_fd, s, sizeof(s));
+  if(read(socket_fd, s, sizeof(s))<=0)
+  {
+    printf("[Client] Failed to read.\n");
+    break;
+  }
+
+  int x, y;
+  if(s[0] == ':') {
+    x = letter_to_num(s[1]);
+    y = letter_to_num(s[2]);
+
+    if(user_board[x][y] != 0) {
+      strncpy(s, ship(user_board[x][y]), 100);
+    }
+
+    user_board[x][y] = 6;
+    attack_board[x][y] = 6;
+  }
+
+}
+
+char* ship(int id) {
+  swtich(id) {
+    case 1:
+      return "You've hit the carrier!\n";
+      break;
+    case 2:
+      return "You've hit the battleship!\n";
+      break;
+    case 3:
+      return "You've hit the cruiser!\n";
+      break;
+    case 4:
+      return "You've hit the submarine!\n";
+      break;
+    case 5:
+      return "You've hit the destroyer!\n";
+      break;
+    case 6:
+      return "You already shot there, try another square.\n";
+      break;
+    default:
+      return "Miss!";
+      break;
+  }
+}
---
+++ game.h
@@ -0,0 +1,2 @@
+int game(int user_board[10][10], int attack_board[10][10], int socket_fd);
+char* ship(int id);
